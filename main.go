5bee8e97dde005a6d61efea1ef8e1c81e56c852a
olegy2010 <olegy2010@ya.ru>
2022-02-18 16:32:15 +0300

    test

--- main.go
+++ main.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/kyokomi/emoji"
 )
 
 func main() {
-	message := emoji.Sprint("Hello :world_map:!")
-	fmt.Println(message)
+	emoji.Sprint("Hello :world_map:!")
 }
